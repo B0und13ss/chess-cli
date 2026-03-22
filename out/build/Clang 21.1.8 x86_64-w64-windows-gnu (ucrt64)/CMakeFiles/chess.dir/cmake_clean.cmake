a947a0e88e227ea15e9e35fe8e6d94c53453a8d2
B0und13ss <idougherty0706@gmail.com>
2026-03-22 19:51:16 -0400

    initial commit, project just created

---
+++ out/build/Clang 21.1.8 x86_64-w64-windows-gnu (ucrt64)/CMakeFiles/chess.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C:/dev/chess-cli/chess/bin/chess.exe"
+  "C:/dev/chess-cli/chess/bin/chess.exe.manifest"
+  "C:/dev/chess-cli/chess/bin/chess.pdb"
+  "CMakeFiles/chess.dir/main.c.obj"
+  "CMakeFiles/chess.dir/main.c.obj.d"
+  "libchess.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
