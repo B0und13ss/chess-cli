e537925527b2bf0b9c4abc4e8de8e519e4c40d3a
B0und13ss <idougherty0706@gmail.com>
2026-03-27 21:14:29 -0400

    kinda splimplified dynamic os code

--- src/main.c
+++ src/main.c
@@ -5,23 +5,27 @@
 #include <stdlib.h>
 #include <wchar.h>
 
-// ! Learn DOXYGEN and comment YOUR FUNCTIONS
-
-int main(void) {
-  // wprintf(L"Hello, from chess! %lc\n", L'\ue261');
 #if defined(_WIN32) || defined(_WIN64)
 #include <fcntl.h>
 #include <io.h>
 #include <windows.h>
+#elif defined(__APPLE__)
+#include <locale.h>
+#elif defined(__linux__)
+#endif
+
+// ! Learn DOXYGEN and comment YOUR FUNCTIONS
+
+int main(void) {
+#if defined(_WIN32) || defined(_WIN64)
   // This is some windows garbage to allow us to print unicode to the terminal
   _setmode(_fileno(stdout), _O_U16TEXT);
 #elif defined(__APPLE__)
-#include <locale.h>
   // Set the C locale so wide-character functions know we're using UTF-8
   setlocale(LC_ALL, "");
 #elif defined(__linux__)
-#else
 #endif
+  // wprintf(L"Hello, from chess! %lc\n", L'\ue261');
 
   Board board = {0};
   initBoard(board);
